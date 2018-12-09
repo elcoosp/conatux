796e41e6c49f219a9869c8096fecbb78c6386cf6
elcoosp <37814441+elcoosp@users.noreply.github.com>
2018-12-09 21:40:45 +0100

    Initialize prisma (#28)
    * chore(server): add generated server files to ignored files
    
    * chore(server): add npm scripts to bootstrap back and front and remove build:babel
    
    * chore(server): add readme
    
    * feat(server): initialize prisma and add the api schema + resolvers for user query and createUser mutation
    
    * chore(server): add nodemon, prisma/graphql-yoga dependencies and start/watch scripts

---
+++ packages/back/server/src/index.js
@@ -0,0 +1,16 @@
+const { prisma } = require("../generated/prisma-client")
+const { GraphQLServer } = require("graphql-yoga")
+const resolvers = require("./resolvers")
+
+const main = () => {
+  const server = new GraphQLServer({
+    typeDefs: "./schema.graphql",
+    resolvers,
+    context: {
+      prisma
+    }
+  })
+  server.start(() => console.log("Server is running on http://localhost:4000"))
+}
+
+main()
