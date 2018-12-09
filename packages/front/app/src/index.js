e919a8e0bbc45136dc4f679b72d89ed5432e955c
Louis Cospain <elcoosp@gmail.com>
2018-12-09 03:11:54 +0100

    style(integrate prettier with eslint, fix files with semi, and add ci task):

--- packages/front/app/src/index.js
+++ packages/front/app/src/index.js
@@ -1,8 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react"
+import ReactDOM from "react-dom"
 
 const App = () => <h1>App</h1>
 
-ReactDOM.render(
-  <App />, document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector("#root"))
