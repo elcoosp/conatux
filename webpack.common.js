fb9d0f5746c8665badbded356f373a138808d6c7
Louis Cospain <elcoosp@gmail.com>
2018-12-09 02:30:03 +0100

    build(webpack): add css/file/babel loaders, html/clean dist folder plugin, source-maps, devserver an

---
+++ webpack.common.js
@@ -0,0 +1,57 @@
+
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const appPath = (...pathToFile) => path.join(__dirname, 'packages', 'front', 'app', ...pathToFile)
+
+const output = {
+  path: path.resolve(__dirname, 'dist'),
+  filename: '[name]/bundle.js'
+}
+
+const rules = [
+  {
+    test: /\.css$/,
+    use: [
+      'style-loader',
+      'css-loader'
+    ]
+  },
+  {
+    test: /\.(png|svg|jpg|gif)$/,
+    use: [
+      'file-loader'
+    ]
+  },
+  {
+    test: /\.(woff|woff2|eot|ttf|otf)$/,
+    use: [
+      'file-loader'
+    ]
+  },
+  {
+    test: /\.(js|jsx)$/,
+    exclude: /node_modules/,
+    loader:
+      'babel-loader',
+    options: {
+      rootMode: 'upward',
+      presets: ['@babel/react']
+    }
+  }
+]
+
+const plugins = [new HtmlWebpackPlugin({
+  template: appPath('public', 'index.html')
+})]
+
+const devServer = {
+  hot: true
+}
+module.exports = {
+  entry: appPath('src', 'index.js'),
+  output,
+  module: { rules },
+  devServer,
+  plugins
+}
