16e6826dc979308494c3cf467ff3f580262e7997
Louis Cospain <elcoosp@gmail.com>
2018-12-29 00:19:04 +0100

    feat(app): add a login page skeleton

---
+++ packages/front/app/src/screens/auth/Login.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Screen } from '@:/screens'
+import { ROUTES } from '@:/constants'
+
+const Login = () => <Screen title={ROUTES.Auth.Login.label} />
+
+export default Login
