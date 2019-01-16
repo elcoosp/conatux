4e1387d922651f569c2add0061f0d91efb86d76b
Louis Cospain <elcoosp@gmail.com>
2019-01-16 22:03:37 +0100

    style(ui): add input styles barebones

--- packages/front/ui/form/Input.js
+++ packages/front/ui/form/Input.js
@@ -1,7 +1,20 @@
 import styled from 'styled-components'
 
+const radius = {
+  sm: '3px'
+}
+const shadow = {
+  sm: '3px 3px 8px 2px rgba(0,0,0,.1)'
+}
+const spacing = {
+  md: '10px'
+}
 const Input = styled.input`
-  margin: 10px;
+  margin: ${spacing.md};
+  padding: ${spacing.md};
+  border: 0px;
+  border-radius: ${radius.sm};
+  box-shadow: ${shadow.sm};
 `
 
 export default Input
