4177efa82c9198d1f9f682fd4cbac6e8aad7df94
Louis Cospain <elcoosp@gmail.com>
2019-01-29 23:31:29 +0100

    feat(app,ui): add a Layout component which inject globalStyles defined in the ui package and wrap ch

--- packages/front/app/src/comp/Layout.js
+++ packages/front/app/src/comp/Layout.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import P from 'prop-types'
 
+import globalStyles from 'ui/globalStyles'
+
+globalStyles()
+
 const Layout = ({ children }) => <section>{children}</section>
 
 Layout.propTypes = {
