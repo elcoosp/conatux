76f7412fd8832e41f84286c6fac7934f8fc3d928
Louis Cospain <elcoosp@gmail.com>
2019-01-29 23:47:55 +0100

    refactor(ui/form): remove input styles since we are gonna use ThemeProvider

--- packages/front/ui/form/Input.js
+++ packages/front/ui/form/Input.js
@@ -1,20 +1,7 @@
 import styled from 'styled-components'
 
-const radius = {
-  sm: '3px'
-}
-const shadow = {
-  sm: '3px 3px 8px 2px rgba(0,0,0,.1)'
-}
-const spacing = {
-  md: '10px'
-}
 const Input = styled.input`
-  margin: ${spacing.md};
-  padding: ${spacing.md};
-  border: 0px;
-  border-radius: ${radius.sm};
-  box-shadow: ${shadow.sm};
+  margin: 10px;
 `
 
 export default Input
