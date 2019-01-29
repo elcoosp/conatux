bc51e4cf977146592d4711edc076ccf28672192e
Louis Cospain <elcoosp@gmail.com>
2019-01-29 23:25:04 +0100

    build(package,jest): remove @babel/plugin-proposal-optional-chaining and add setup files to mock loc

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ module.exports = {
   },
   snapshotSerializers: ['enzyme-to-json/serializer'],
   testPathIgnorePatterns: ['/node_modules/', '/cypress/'],
+  setupFiles: ['./setupJest.js', 'jest-localstorage-mock'],
   setupTestFrameworkScriptFile: 'jest-enzyme',
   testEnvironment: 'enzyme'
 }
