52de93b370d32a156b9db93371a460814a349f5b
Louis Cospain <elcoosp@gmail.com>
2019-01-29 23:38:22 +0100

    refactor(app): add mutationOptions to  handleMutationSubmit

--- packages/front/app/src/comp/helpers/form/handleMutationSubmit.js
+++ packages/front/app/src/comp/helpers/form/handleMutationSubmit.js
@@ -1,24 +1,33 @@
 /* namedMutationObj: An object with a unique property which is the name of the mutation and whose value is a mutation function */
-export const execNamedMutation = async (namedMutationObj, variables) => {
+export const execNamedMutation = async (
+  namedMutationObj,
+  mutationOptions = {},
+  variables
+) => {
   const [mutationName] = Object.keys(namedMutationObj)
   const mutation = namedMutationObj[mutationName]
 
   const {
     data: { [mutationName]: response } // Extract the relevant response data from an apollo result
   } = await mutation({
-    variables
+    variables,
+    ...mutationOptions
   })
 
   return response
 }
 
 // Handle submitting an apollo mutation from a formik onSubmit event
-const handleMutationSubmit = namedMutationObj => async (
+const handleMutationSubmit = (namedMutationObj, mutationOptions) => async (
   variables, // Mutation variables
   { setSubmitting, setErrors } // Formik handler function
 ) => {
   try {
-    const response = await execNamedMutation(namedMutationObj, variables)
+    const response = await execNamedMutation(
+      namedMutationObj,
+      mutationOptions,
+      variables
+    )
     return response
   } catch (error) {
     // TODO: handle error mappings to fields with setErrors
