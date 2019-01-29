c8c9bdda9bbb1b49ceb74a5d33b9d0d1fd6041d1
Louis Cospain <elcoosp@gmail.com>
2019-01-29 23:36:51 +0100

    refactor(app): extract isDisabled functioinnality, make mutation optional and add Errors component h

--- packages/front/app/src/comp/helpers/form/Form.js
+++ packages/front/app/src/comp/helpers/form/Form.js
@@ -2,22 +2,49 @@ import React from 'react'
 import P from 'prop-types'
 import { Formik, Form as FormikForm } from 'formik'
 import { Form as UiForm, Submit } from 'ui/form'
+import Errors from '@:/comp/helpers/Errors'
+import getGqlErrorMessages from './getGqlErrorMessages'
+import handleMutationSubmit from './handleMutationSubmit'
 
-const FormInner = UiForm.withComponent(FormikForm)
+export const FormInner = UiForm.withComponent(FormikForm)
 
-const Form = ({ children, ...props }) => (
-  <Formik {...props}>
+export const isDisabled = (isSubmitting, errors = {}) =>
+  isSubmitting || Object.keys(errors).length > 0
+
+const Form = ({
+  children,
+  validationSchema,
+  mutation,
+  mutationOptions,
+  apolloError,
+  ...props
+}) => (
+  <Formik
+    {...(mutation
+      ? { onSubmit: handleMutationSubmit(mutation, mutationOptions) }
+      : {})}
+    validationSchema={validationSchema}
+    initialValues={validationSchema.cast()}
+    {...props}
+  >
     {({ isSubmitting, errors }) => (
       <FormInner>
         {children}
-        <Submit disabled={isSubmitting || Object.keys(errors).length > 0} />
+        <Submit disabled={isDisabled(isSubmitting, errors)} />
+        <Errors errors={getGqlErrorMessages(apolloError)} />
       </FormInner>
     )}
   </Formik>
 )
 
 Form.propTypes = {
-  children: P.node.isRequired
+  children: P.node.isRequired,
+  validationSchema: P.shape({
+    cast: P.func.isRequired
+  }).isRequired, // Yup object
+  mutation: P.object, // A named mutation object
+  mutationOptions: P.object, // Mutation options like update
+  apolloError: P.object
 }
 
 export default Form
